2df252728a29a5a4528de7ecd07e0181062b4c21
SereMotsatt <sebabasvelazquez@gmail.com>
2025-10-21 03:42:26 -0430

    Se añadió el oro al juego

--- mods/default/automated.lua
+++ mods/default/automated.lua
@@ -20,6 +20,7 @@ cwoodt = ":-160:-50:-70"
 cstonet = ":0:-100:-50"
 csteelt = ":0:-100:0"
 ccoppert = ":-160:-50:-40"
+cgoldt = ":-140:20:-10"
 cmesitet = ":-150:-10:0"
 
 damasco_b_t = '[mask:default_damascus_pattrn.png^[hsl:0:0:25'
@@ -31,6 +32,7 @@ damasco_t = 'default_damascus_pattrn.png^[hsl:0:0:25'
 
 materials.register_material(nil, "steel", "Steel", csteelt)
 materials.register_material(nil, "copper", "Copper", ccoppert)
+materials.register_material(nil, "gold", "Gold", cgoldt)
 materials.register_material(nil, "damascus_steel", "Damascus Steel", csteelt, damasco_b_t)
 materials.register_material(nil, "mesite", "Mesite", cmesitet, nil, 8)
 
@@ -39,9 +41,10 @@ materials.register_material(nil, "mesite", "Mesite", cmesitet, nil, 8)
 --
 
 tools.register_tools("wood", "Wooden", cwoodt, 1.40, 5)
-tools.register_tools("stone", "Stone", cstonet, 1.00, 15)
+tools.register_tools("stone", "Stone", cstonet, 1.00, 12)
 tools.register_tools("steel", "Steel", csteelt, 0.60, 20)
-tools.register_tools("copper", "Copper", ccoppert, 0.80, 10)
+tools.register_tools("copper", "Copper", ccoppert, 0.80, 15)
+tools.register_tools("gold", "Gold", cgoldt, 0.70, 10)
 tools.register_tools("damascus_steel", "Damascus Steel", csteelt, 0.55, 40, damasco_t)
 tools.register_tools("mesite", "Mesite", cmesitet, 0.45, 35)
 
@@ -53,5 +56,6 @@ crafttools.register_crafttools("wood", false)
 crafttools.register_crafttools("stone", false, "cobble")
 crafttools.register_crafttools("steel")
 crafttools.register_crafttools("copper")
+crafttools.register_crafttools("gold")
 crafttools.register_crafttools("damascus_steel")
 crafttools.register_crafttools("mesite")
